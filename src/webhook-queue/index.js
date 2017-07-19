aba84e7ace304f53007d37a25e02afb43b0406f8
Jef King <jefkin@microsoft.com>
2017-07-19 09:32:26 -0400

    HTTP Status codes

--- src/webhook-queue/index.js
+++ src/webhook-queue/index.js
@@ -3,8 +3,10 @@ module.exports = function (context, req) {
 
     if (typeof req.body != 'undefined' && typeof req.body == 'object') {
         context.bindings.out = req.body;
+        context.res.sendStatus(200);
     }
     else {
+        context.res.sendStatus(400);
         error = "no data; or invald payload in body";
     }
 
